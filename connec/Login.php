3ae3facb0909dc02de8c58861d30555fdea9d729
Mickil1 <momolemoche@gmail.com>
2020-11-28 16:57:32 +0000

    fiche login fini

---
+++ connec/Login.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login</title>
+</head>
+<body>
+<form action="" method="get" class="Login">
+ <div class="identifiant">
+    <label for="pseudo">Entre ton pseudo</label>
+    <input type="text" id="pseudo" name="pseudo" minlength="2" maxlength="10" size="10"> 
+ </div>
+ <div class="motdepasse">
+   <label for="password">Entre ton mot de passe</label>
+   <input type="password" id="password" name="password" minlength="2" maxlength="10" size="10"> 
+ </div>
+ <div class="Valider">
+    <input type="submit" value="Connecter">
+ </div>
+</form>
+  
+  
+</body>
+</html>
