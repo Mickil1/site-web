bd8b3f02418412e7d0ff91b2ea71d12c54b7cd98
Mickil1 <momolemoche@gmail.com>
2021-01-06 09:08:26 +0000

    test 2

--- Include/javascript/main.js
+++ Include/javascript/main.js
@@ -1,11 +1,12 @@
+$('map').imageMapResize();
 
 //pop-up map
 $('area').each(function(){
     var area = $(this),
         alt = area.attr('alt');
     area.mouseenter(function(){
-        $('#overlay').html(alt);
+        $('#Overlay').html(alt);
     }).mouseleave(function(){
-        $('#overlay').html('');
+        $('#Pverlay').html('');
     });
 });
