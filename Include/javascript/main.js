f1b93b27c88920aa41c7cedeaf754e6bc9f8eed2
Mickil1 <momolemoche@gmail.com>
2021-01-19 15:01:34 +0000

    Town need to be complete

--- Include/javascript/main.js
+++ Include/javascript/main.js
@@ -1,13 +1,32 @@
-
 $('map').imageMapResize();
 
 //pop-up map
+/*
 $('area').each(function(){
     var area = $(this),
         alt = area.attr('alt');
     area.mouseenter(function(){
+        $('#Overlay').css('top')
         $('#Overlay').html(alt);
     }).mouseleave(function(){
-        $('#Pverlay').html('');
+        $('#Overlay').html('');
     });
 });
+*/
+var moveLeft = -30;
+var moveDown = -140;
+
+$('area').each(function(){
+  var area = $(this),
+      alt = area.attr('alt');
+  area.hover(function(e){
+    $('#Overlay').html(alt);
+    $('#Overlay').show();
+  },function(){
+    $('#Overlay').hide();
+  });
+
+  area.mousemove(function(e){
+    $('#Overlay').css('top', e.pageY + moveDown).css('left', e.pageX + moveLeft);
+  });
+})
