233f1be594191f86d3b1cde9e4c66ee266d021b0
Mickil1 <momolemoche@gmail.com>
2021-01-22 07:43:32 +0000

    mise en place de l'organisationn du site

---
+++ View/V_entete.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+
+<head>
+    <title>Réunion project</title>
+<link rel="stylesheet" href="./Include/css/main.css">
+</head>
+
+<body>
+
+<!--Entête-->
+<header>
+
+</header>
+
+<nav>
+    <ul>
+        <li><a href="">Accueil</a></li>
+        <li><a href="./Contenu/History.html">Histoire</a></li>
+        <li><a href="./Contenu/Map.html">Carte</a></li>
+        <li><a href="./Contenu/Race.html">Race</a></li>
+        <li><a href="./Contenu/Town.html">Ville</a></li>
+    </ul>
+</nav>
